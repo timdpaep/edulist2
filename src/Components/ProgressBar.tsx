e90c56de1f7bb4647519bcaa65d86d5ed5790c9e
timdpaep <tim.depaepe@gmail.com>
2021-09-10 16:13:21 +0200

    giving color to progress bar

--- src/Components/ProgressBar.tsx
+++ src/Components/ProgressBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import { useCallback } from 'react'
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
 
@@ -20,11 +20,8 @@ interface IBarProps {
 interface IBarFillingProps {
 	width?: string
 	height?: string
+	bgColor?: string
 }
-
-/**
- * Framer motion
- */
 
 /**
  * Styled Components
@@ -42,10 +39,29 @@ const BarFilling = styled(motion.div)<IBarFillingProps>`
 	position: absolute;
 	top: 0;
 	left: 0;
-	background-color: var(--orange);
+	background-color: ${props =>
+		props.bgColor ? props.bgColor : 'var(--lightGrey)'};
 	width: ${props => (props.width ? props.width : '100%')};
 	height: ${props => (props.height ? props.height : '100%')};
 `
+const getBackgroundColor = (p: number): string => {
+	if (p >= 0 && p < 20) {
+		return 'var(--background-color-20)'
+	}
+	if (p >= 20 && p < 40) {
+		return 'var(--background-color-20)'
+	}
+	if (p >= 40 && p < 60) {
+		return 'var(--background-color-40)'
+	}
+	if (p >= 60 && p < 80) {
+		return 'var(--background-color-60)'
+	}
+	if (p >= 80 && p < 100) {
+		return 'var(--background-color-80)'
+	}
+	return 'var(--background-color-100)'
+}
 
 export const ProgressBar = ({
 	width = '100%',
@@ -54,6 +70,7 @@ export const ProgressBar = ({
 }: IProgressBarProps) => (
 	<Bar width={width} height={height}>
 		<BarFilling
+			bgColor={getBackgroundColor(percentage)}
 			initial={{
 				width: '0',
 			}}
