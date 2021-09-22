bf104f5864f4e8299e8c87033537a9964602ef9c
timdpaep <tim.depaepe@gmail.com>
2021-09-22 22:04:03 +0200

    switched back over to ReactPlayer due to bug in Vime

--- src/Components/YouTubePlayer.tsx
+++ src/Components/YouTubePlayer.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
-import { Player, Youtube, DefaultUi } from '@vime/react'
+// import { Player, Youtube } from '@vime/react'
+import ReactPlayer from 'react-player/lazy'
 
 interface IYouTubePlayerProps {
 	videoId: string
 }
 
 export const YouTubePlayer = ({ videoId }: IYouTubePlayerProps) => (
-	<Player volume={50}>
-		<Youtube videoId={videoId} />
-		<DefaultUi />
-	</Player>
+	<>
+		{/*
+		Leave this, there is a bug in Vime. Waiting until it's fixed.
+		<Player isVideoView volume={50}>
+			<Youtube videoId={videoId} />
+		</Player> */}
+		<ReactPlayer
+			url={`https://www.youtube.com/watch?v=${videoId}`}
+			style={{
+				borderRadius: 5,
+			}}
+			playing
+			className='react-player'
+			width='100%'
+			controls
+			volume={0.5}
+			config={{
+				youtube: {
+					playerVars: {
+						modestbranding: 1,
+						showinfo: 0,
+						showsearch: 0,
+						fs: 1,
+					},
+				},
+			}}
+		/>
+	</>
 )
